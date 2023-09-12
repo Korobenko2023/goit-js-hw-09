327635ef10ca37c5a121349f7d0323cdfb038ae2
Korobenko2023 <130507694+Korobenko2023@users.noreply.github.com>
2023-09-12 14:03:33 +0000

    Deploying to gh-pages from @ Korobenko2023/goit-js-hw-09@49e3562dc4b5295132aa4de642a2db721636e0ee 🚀

---
+++ 01-color-switcher.6a0c2e42.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]"),n=document.body;e.addEventListener("click",(function(){t=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));n.style.backgroundColor=t}),1e3),e.disabled=!0})),a.addEventListener("click",(function(){clearInterval(t),e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.6a0c2e42.js.map
